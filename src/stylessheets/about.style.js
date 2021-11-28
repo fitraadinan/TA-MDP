d2cc947305552648efcd2168bc5259e8cd52dab2
fitraadinan <63963463+fitraadinan@users.noreply.github.com>
2021-11-28 09:40:28 +0700

    Update about.style.js

--- src/stylessheets/about.style.js
+++ src/stylessheets/about.style.js
@@ -1,5 +1,5 @@
 import { Background } from '@react-navigation/elements';
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     container: {
@@ -10,17 +10,26 @@ export default StyleSheet.create({
     headerImage: {
         height: 180,
         width: 320,
-        borderRadius:20,
+        borderRadius: 20,
         marginBottom: 10,
     },
     headerBodyTXT: {
         fontSize: 15,
         textAlign: 'center',
         marginHorizontal: 27,
-        marginVertical: 10,
+        marginVertical: 5,
     },
     headerHello: {
         fontSize: 20,
         fontWeight: 'bold'
-    }
+    },
+    headerLink: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        marginHorizontal: 27,
+        marginHorizontal: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center'
+    },
 });
